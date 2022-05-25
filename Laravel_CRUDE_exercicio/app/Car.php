e333db11a786f0069e7213c7d6232ef266a34c9c
António Barbosa <tobarbosa11@gmail.com>
2022-05-25 23:14:13 +0100

    Initial commit tables done

---
+++ Laravel_CRUDE_exercicio/app/Car.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Car extends Model
+{
+    public function brand(){
+        return $this->belongsTo(Brand::class);
+    }
+}
